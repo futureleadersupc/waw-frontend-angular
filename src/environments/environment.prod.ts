b5db23325043f677b2ca75af59809252884b7378
Diego Albitres <diego.albitres@gmail.com>
2022-07-01 21:31:31 -0500

    chore: change production url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,5 @@ import { Environment } from "./model";
 
 export const environment: Environment = {
   production: true,
-  apiUrlBase:
-    "https://my-json-server.typicode.com/futureleadersupc/waw-backend-json",
+  apiUrlBase: "https://api-os.wawupc.ga/api/v1",
 };
